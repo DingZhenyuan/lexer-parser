0fea73cda15003c885497579cfca976de91cd7f3
DingZhenyuan <ding_zy@outlook.com>
2019-05-15 23:15:59 +0800

    建立parser基本结构

---
+++ parser.py
@@ -0,0 +1,40 @@
+import keywords
+import lexer
+
+
+def factor():
+    pass
+
+
+def expression():
+    pass
+
+
+def yucu():
+    pass
+
+
+def term():
+    pass
+
+
+def statement():
+    pass
+
+
+def lrparser():
+    pass
+
+
+def scaner():
+    pass
+
+
+def main():
+    p = 0
+    k = 0
+    print("please input a string(end with '#')")
+    ch = input()
+
+    pass
+
